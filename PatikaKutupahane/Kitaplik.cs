b39e6976ea142f2354a96b7493cd9c7ade2d0b2c
bburakbbasol <bburakbbasol@gmail.com>
2025-01-09 13:20:04 +0300

    Add project files.

---
+++ PatikaKutupahane/Kitaplik.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace PatikaKutupahane
+{
+	public class Kitaplik
+	{
+		// Properties
+		private string _ad;
+		private string _yazarAdi;
+		private string _yazarSoyadi;
+		private int _sayfaSayisi;
+		private string _yayinEvi;
+		private DateTime _kayit;
+
+		public string Ad
+		{
+			get { return _ad; }
+			set { _ad = value; }
+		}
+
+		public string YazarAdi
+		{
+			get { return _yazarAdi; }
+			set { _yazarAdi = value; }
+		}
+
+		public string YazarSoyadi
+		{
+			get { return _yazarSoyadi; }
+			set { _yazarSoyadi = value; }
+		}
+
+		public int SayfaSayisi
+		{
+			get { return _sayfaSayisi; }
+			set { _sayfaSayisi = value; }
+		}
+
+		public string YayinEvi
+		{
+			get { return _yayinEvi; }
+			set { _yayinEvi = value; }
+		}
+
+		public DateTime Kayit
+		{
+			get { return _kayit; }
+			set { _kayit = value; }
+		}
+		// Default constructor (parametre almayan)
+		public Kitaplik()
+		{
+			_kayit= DateTime.Now;
+
+		}
+		//Methot
+		public void KitapBilgi()
+		{
+			Console.WriteLine($"Kitabın adı {_ad} kitapın yazarı {_yazarAdi} {YazarSoyadi} kitabin sayfa sayisi: {SayfaSayisi}, kitabin yayin evi:{_yayinEvi},kitabın kayit tarihi:{_kayit}");
+		}
+		public static void KitaplikBilgi()
+		{
+
+
+		}
+
+		//Parametre alan Constructor
+		public Kitaplik(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinEvi)
+		{
+			_ad = ad;
+			_yazarAdi = yazarAdi;
+			_yazarSoyadi = yazarSoyadi;
+			_sayfaSayisi = sayfaSayisi;
+			_yayinEvi = yayinEvi;
+			_kayit = DateTime.Now; 
+		}
+	}
+}
---
+++ PatikaKutupahane/Program.cs
@@ -0,0 +1,13 @@
+﻿using PatikaKutupahane;
+// Parametreli constructor kullanarak kitap nesnesi oluşturma
+Kitaplik Kitap1 = new Kitaplik("Bir Kitap", "Ahmet", "Yılmaz",320,"Alfa Yayınları");
+// Default constructor kullanarak kitap nesnesi oluşturma
+Kitaplik Kitap2 = new Kitaplik();
+
+Kitap2.Ad = "Başka Bir Kitap";
+Kitap2.YazarAdi = "Ayşe";
+Kitap2.YazarSoyadi = "Karan";
+Kitap2.SayfaSayisi = 215;
+Kitap2.YayinEvi = "Beta Yayin Evi";
+Kitap1.KitapBilgi();
+Kitap2.KitapBilgi();
